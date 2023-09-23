054c6485ec68582040968e3ff9b1b3dc8e792ebe
ma2za <mazzapaolo2019@gmail.com>
2023-09-23 22:45:24 +0200

    👺 voice journal

--- src/telegram_llm_bot/app.py
+++ src/telegram_llm_bot/app.py
@@ -4,10 +4,6 @@ import importlib
 import logging.config
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv("../../.env")
-load_dotenv("bots/base_chatbot/.env")
 settings = importlib.import_module(os.getenv("SETTINGS_FILE"))
 
 logging.config.fileConfig("../../logging.conf", disable_existing_loggers=False)
